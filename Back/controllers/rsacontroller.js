e249f6613dda1a83b7525f246a40ad8c9604a464
Juanjosedelriomolano <juan.delrio00@usc.edu.co>
2024-09-13 16:23:12 -0500

    Create rsacontroller.js

---
+++ Back/controllers/rsacontroller.js
@@ -0,0 +1,42 @@
+const rsaModel = require('../models/rsa');
+
+// Ruta para calcular n
+exports.calculateN = (req, res) => {
+    const { p, q } = req.body;
+    const n = rsaModel.calculateN(p, q);
+    res.json({ n });
+};
+
+// Ruta para calcular e
+exports.calculateE = (req, res) => {
+    const { p, q } = req.body;
+    if (!p || !q || p === q) {
+        return res.status(400).json({ error: 'p y q deben ser primos diferentes' });
+    }
+    const possibleE = rsaModel.calculateE(p, q);
+    res.json({ possibleE });
+};
+
+// Ruta para calcular d
+exports.calculateD = (req, res) => {
+    const { p, q, e } = req.body;
+    if (!p || !q || !e || p === q) {
+        return res.status(400).json({ error: 'p, q, y e deben ser válidos y p y q deben ser primos diferentes' });
+    }
+    const d = rsaModel.calculateD(p, q, e);
+    if (d !== undefined) {
+        res.json({ d });
+    } else {
+        res.status(500).json({ error: 'Error al calcular d' });
+    }
+};
+
+// Ruta para convertir el mensaje a números ASCII
+exports.convertMessage = (req, res) => {
+    const { message } = req.body;
+    if (typeof message !== 'string') {
+        return res.status(400).json({ error: 'Mensaje no proporcionado' });
+    }
+    const asciiMessage = rsaModel.convertMessage(message);
+    res.json({ asciiMessage });
+};
